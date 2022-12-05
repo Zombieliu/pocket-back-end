12a156e749ac719f7ad1998b84c5227619b69679
henryliu <17600047539@163.com>
2022-12-05 15:45:50 +0800

    update

--- src/chain/index.ts
+++ src/chain/index.ts
@@ -1,7 +1,7 @@
 import { ApiPromise,HttpProvider } from '@polkadot/api';
 
 const chain_api = async () =>{
-    const provider = new HttpProvider('http://localhost:9933');
+    const provider = new HttpProvider('https://devnet.web3games.org/evm');
     const api = await ApiPromise.create({ provider });
     return api;
 }
