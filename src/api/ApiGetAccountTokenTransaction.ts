7695b8ffac0ebbb1cef1d5d49b5096553bfd7d5b
henryliu <17600047539@163.com>
2022-12-29 21:56:53 +0800

    update

--- src/api/ApiGetAccountTokenTransaction.ts
+++ src/api/ApiGetAccountTokenTransaction.ts
@@ -4,6 +4,7 @@ import {
     ReqGetAccountTokenTransaction,
     ResGetAccountTokenTransaction
 } from "../shared/protocols/PtlGetAccountTokenTransaction";
+import {hexToString} from '@polkadot/util'
 
 export default async function (call: ApiCall<ReqGetAccountTokenTransaction, ResGetAccountTokenTransaction>) {
     // Error
@@ -16,7 +17,7 @@ export default async function (call: ApiCall<ReqGetAccountTokenTransaction, ResG
 
     const explorer_api = axios.create({
         baseURL: 'https://explorer-testnet-restful-api.web3games.org',
-        timeout: 9999,
+        timeout: 99999,
         headers:{
             accept: 'application/json'
         }
@@ -31,23 +32,36 @@ export default async function (call: ApiCall<ReqGetAccountTokenTransaction, ResG
 
     const data_list = JSON.parse(data.data.res.content);
 
-
+    // console.log(data_list)
     if(data_list.total == 0 ){
         await call.error('Content is empty');
         return;
     }else{
             let new_data_list = []
             for (let i = 0; i < data_list.total; i++){
-                const data = data_list.items[i];
-                data.tokenName = 'W3G';
-                data.decimals = '18';
-                delete data.tokenFungibleCreated;
-                delete data.fungibleTokenId;
-                delete data.blockNum;
-                delete data.eventIndex;
-                delete data.extrinsicIndex;
-                new_data_list.push(data);
-            }
+                const data = data_list.items[i]
+                if (fungibleTokenId !== "-1"){
+                    const symbol = data_list.items[i].tokenFungibleCreated.symbol
+                    const decimals = data_list.items[i].tokenFungibleCreated.decimals
+                    data.tokenName = hexToString(symbol)
+                    data.decimals =  decimals
+                    delete data.tokenFungibleCreated
+                    delete data.fungibleTokenId
+                    delete data.blockNum
+                    delete data.eventIndex
+                    delete data.extrinsicIndex
+                    new_data_list.push(data)
+                }else{
+                    data.tokenName = 'W3G'
+                    data.decimals = '18'
+                    delete data.tokenFungibleCreated
+                    delete data.fungibleTokenId
+                    delete data.blockNum
+                    delete data.eventIndex
+                    delete data.extrinsicIndex
+                    new_data_list.push(data)
+                }
+            };
             await call.succ({
                 time: time,
                 data_list:JSON.stringify(new_data_list)
